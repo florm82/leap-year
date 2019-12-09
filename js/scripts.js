7da2238ba9a69a5a0c0ad7935b7a848c5f1adc96
Flor Nazari <florm82@gmail.com>
2019-12-09 15:45:12 -0800

    add final leap year functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,10 @@
 var leapYear = function(year) {
-  if (year % 100 === 0) {
-    return false;
-  } else if (year % 4 === 0) {
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
     return true;
   } else {
     return false;
   }
 };
-
 
 //jQuerry interface logic is here:
 $(document).ready(function() {
