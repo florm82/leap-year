0a987f4e00cd21c2d3d593a0ea064be54594e430
Flor Nazari <florm82@gmail.com>
2019-12-11 23:51:13 -0800

    add some functions

--- js/scripts.js
+++ js/scripts.js
@@ -7,8 +7,15 @@ var leapYear = function(year) {
     return false;
   }
 };
+var factorial = function(number) {
+  if (number <= number * (number-1) && (number > 0 )) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
-// user interface logic
+}// user interface logic
 $(document).ready(function() {
   $("form#leap-year").submit(function(event) {
     event.preventDefault();
@@ -16,6 +23,24 @@ $(document).ready(function() {
     var result = leapYear(year);
 
     $(".year").text(year);
+
+    if (!result) {                 // same as writing if (result === false)
+      $(".not").text("not");
+    } else {
+      $(".not").text("");
+    }
+
+    $("#result").show();
+  });
+});
+
+$(document).ready(function() {
+  $("form#number").submit(function(event) {
+    event.preventDefault();
+    var number = parseInt($("input#number").val());
+    var result = factorial(number);
+
+    $(".number").text(number);
 
     if (!result) {                 // same as writing if (result === false)
       $(".not").text("not");
